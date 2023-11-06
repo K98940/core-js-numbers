01c066b4c2180a7cc51025781d6a21eb0938bd62
k98940 <pv1xxb@yandex.ru>
2023-11-06 14:48:59 +0600

    feat: the task done. (Mon, Nov 06, 2023 02:48:59 PM)

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -556,8 +556,8 @@ function getSumOfNumbers(...args) {
  * -5, -6 => -5
  * 0, 5   => 5
  */
-function getMaxNumber(/* firstNumber, secondNumber */) {
-  throw new Error('Not implemented');
+function getMaxNumber(...args) {
+  return Math.max(...args);
 }
 
 /**
@@ -572,8 +572,9 @@ function getMaxNumber(/* firstNumber, secondNumber */) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  const rnd = Math.random() * (max - min);
+  return Math.round(rnd + min);
 }
 
 /**
@@ -586,8 +587,8 @@ function getRandomInteger(/* min, max */) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -603,8 +604,14 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  const absNumber = Math.abs(number);
+  let oddCount = 0;
+
+  for (let i = 0; i <= absNumber; i += 1) {
+    if (i % 2 !== 0) oddCount += 1;
+  }
+  return oddCount;
 }
 
 module.exports = {
